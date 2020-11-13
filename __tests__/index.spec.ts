0378824b6270df8c02c4d803b373aefb60067445
斎藤基樹 <stmtk13044032@gmail.com>
2020-11-13 10:23:51 +0900

    指摘された箇所を直した

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -3,7 +3,6 @@ import { ConfigFile } from '../src/getConfig'
 import build from '../src'
 import path from 'path'
 
-// returns path string array
 function readDirRecursive(dirPath: string): string[] {
   return fs.readdirSync(dirPath, { withFileTypes: true })
     .map(file => file.isDirectory() ? readDirRecursive(path.join(dirPath, file.name)) : [path.join(dirPath, file.name)])
@@ -14,26 +13,6 @@ describe('cli test', () => {
   beforeAll(() => fs.mkdirSync('_samples'))
   afterAll(() => fs.promises.rmdir('_samples', { recursive: true }))
 
-  test('main', () => {
-    const configs: ConfigFile[] = require('../aspida.config.js')
-
-    return Promise.all(
-      configs.map(async config => {
-        await build({
-          ...config,
-          input: `_${config.input}`
-        })[0]
-
-        expect(fs.readFileSync(`_${config.input}/$api.ts`, 'utf8')).toBe(
-          fs.readFileSync(`${config.input}/$api.ts`, 'utf8').replace(/\r/g, '')
-        )
-
-        expect(fs.readFileSync(`_${config.input}/@types.ts`, 'utf8')).toBe(
-          fs.readFileSync(`${config.input}/@types.ts`, 'utf8').replace(/\r/g, '')
-        )
-      })
-    )
-  }),
   test('inspection', () => {
     const configs: ConfigFile[] = require('../aspida.config.js')
 
@@ -46,7 +25,7 @@ describe('cli test', () => {
 
         for (const filePath of readDirRecursive(config.input)) {
           expect(fs.readFileSync(`_${filePath}`, 'utf8')).toBe(
-            fs.readFileSync(`${filePath}`, 'utf8').replace(/\r/g, '')
+            fs.readFileSync(filePath, 'utf8').replace(/\r/g, '')
           )
         }
       })
