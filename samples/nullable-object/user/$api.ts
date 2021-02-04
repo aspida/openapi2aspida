cef039206fcddada7bf23b15be65382675aac944
redshoga <yui.ichioka.contact@gmail.com>
2021-02-04 18:26:18 +0900

    anyOf + nullableのサンプルのswaggerの追加

---
+++ samples/nullable-object/user/$api.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import { AspidaClient, BasicHeaders } from 'aspida'
+import { Methods as Methods0 } from '.'
+
+const api = <T>({ baseURL, fetch }: AspidaClient<T>) => {
+  const prefix = (baseURL === undefined ? '' : baseURL).replace(/\/$/, '')
+  const PATH0 = '/user'
+  const GET = 'GET'
+
+  return {
+    get: (option?: { config?: T }) =>
+      fetch<Methods0['get']['resBody'], BasicHeaders, Methods0['get']['status']>(prefix, PATH0, GET, option).json(),
+    $get: (option?: { config?: T }) =>
+      fetch<Methods0['get']['resBody'], BasicHeaders, Methods0['get']['status']>(prefix, PATH0, GET, option).json().then(r => r.body),
+    $path: () => `${prefix}${PATH0}`
+  }
+}
+
+export type ApiInstance = ReturnType<typeof api>
+export default api
