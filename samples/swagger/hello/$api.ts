3692baff8e98a036f8427a8fc41c74f7ef1f8034
Luma <markermails@gmail.com>
2021-08-22 16:05:38 +0900

    fix: include response schemas that has no application/ prefixed content-typent-type (#154)
    * fix: include response schemas that has no application/ prefixed content-type
    
    * chore: run lint:fix

---
+++ samples/swagger/hello/$api.ts
@@ -0,0 +1,32 @@
+/* eslint-disable */
+// prettier-ignore
+import { AspidaClient, BasicHeaders, dataToURLString } from 'aspida'
+// prettier-ignore
+import { Methods as Methods0 } from '.'
+
+// prettier-ignore
+const api = <T>({ baseURL, fetch }: AspidaClient<T>) => {
+  const prefix = (baseURL === undefined ? 'https://petstore.swagger.io/v2' : baseURL).replace(/\/$/, '')
+  const PATH0 = '/hello'
+  const GET = 'GET'
+
+  return {
+    /**
+     * @returns returns a greeting
+     */
+    get: (option?: { query?: Methods0['get']['query'], config?: T }) =>
+      fetch<Methods0['get']['resBody'], BasicHeaders, Methods0['get']['status']>(prefix, PATH0, GET, option).text(),
+    /**
+     * @returns returns a greeting
+     */
+    $get: (option?: { query?: Methods0['get']['query'], config?: T }) =>
+      fetch<Methods0['get']['resBody'], BasicHeaders, Methods0['get']['status']>(prefix, PATH0, GET, option).text().then(r => r.body),
+    $path: (option?: { method?: 'get'; query: Methods0['get']['query'] }) =>
+      `${prefix}${PATH0}${option && option.query ? `?${dataToURLString(option.query)}` : ''}`
+  }
+}
+
+// prettier-ignore
+export type ApiInstance = ReturnType<typeof api>
+// prettier-ignore
+export default api
