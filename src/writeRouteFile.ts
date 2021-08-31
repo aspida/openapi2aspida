36d87a137eee2fccea84b9d25f8f3acc40788560
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-08-31 14:01:06 +0900

    -o で指定したフォルダにファイルを作成できるようになった

--- src/writeRouteFile.ts
+++ src/writeRouteFile.ts
@@ -22,25 +22,27 @@ export default ({
   outputDir }`,
     config,
     types,
-    files,
+
     outputDir
   )
   if (types) {
-    fs.mkdirSync(`${config.input}/@types`)
-    fs.writeFileSync(`${config.input}/@types/index.ts`, types, 'utf8')
+    fs.mkdirSync(`${outputDir}/@types`)
+    fs.writeFileSync(`${outputDir}/@types/index.ts`, types, 'utf8')
   }
 
   files.forEach(p => {
     const fileName = p.file.pop()
     p.file.forEach((_d, i, dirList) => {
-      const dirPath = `${config.input}/${dirList.slice(0, i + 1).join('/')}`
+      const dirPath = `${outputDir}/${dirList.slice(0, i + 1).join('/')}`
       if (!fs.existsSync(dirPath)) {
         fs.mkdirSync(dirPath)
       }
     })
 
-    fs.writeFileSync(`${config.input}/${p.file.join('/')}/${fileName}.ts`, p.methods, 'utf8')
+    fs.writeFileSync(`${outputDir}/${p.file.join('/')}/${fileName}.ts`, p.methods, 'utf8')
   })
-
-  build(config)
+  const buildConfig = config
+  buildConfig.input = outputDir || config.input
+  console.log(`🚀 ~ file: writeRouteFile.ts ~ line 46 ~ config`, config)
+  build(buildConfig)
 }
