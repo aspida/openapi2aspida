b8082321d932fff5dacd5d35c8f09360262e1313
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-08-31 11:36:54 +0900

    テストを追加

---
+++ __tests__/outputFilePath.spec.ts
@@ -0,0 +1,27 @@
+import outputFilePath from '../src/outputFilePath'
+
+describe('ファイルパスの検査', () => {
+  test('ファイルパスが正しいこと', () => {
+    expect(
+      outputFilePath({
+        InputFilepath: 'apiapi',
+        cliOutputPath: 'apiapi/apiapi/apiapi'
+      })
+    ).toBe('apiapi/apiapi/apiapi')
+  })
+  test('cli の -oが null', () => {
+    expect(
+      outputFilePath({
+        InputFilepath: 'apiapi',
+        cliOutputPath: undefined
+      })
+    ).toBe('apiapi/apiapi/apiapi')
+  })
+  test('cli の -oが指定されていない', () => {
+    expect(
+      outputFilePath({
+        InputFilepath: 'apiapi'
+      })
+    ).toBe('apiapi')
+  })
+})
